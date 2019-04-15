84931543afd9ec57f917d6f051aea2f958981a20
minny96 <79872340869@ya.ru>
2019-04-15 18:24:08 +0300

    Попытка соединить PostgreSQL с PHP

---
+++ testdb.php
@@ -0,0 +1,26 @@
+<?php
+// Соединение, выбор базы данных
+$dbconn = pg_connect("host=localhost dbname=hotelcrm user=hotel password=hotel")
+    or die('Не удалось соединиться: ' . pg_last_error());
+
+// Выполнение SQL-запроса
+$query = 'SELECT * FROM guest';
+$result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
+
+// Вывод результатов в HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Очистка результата
+pg_free_result($result);
+
+// Закрытие соединения
+pg_close($dbconn);
+?>
