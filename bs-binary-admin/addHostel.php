48a4a2342690d42ea8367a60ec4f7f74fb9720bb
minny96 <79872340869@ya.ru>
2019-04-25 20:05:43 +0300

    Добавление отеля из веба

---
+++ bs-binary-admin/addHostel.php
@@ -0,0 +1,9 @@
+<?php
+// Соединение, выбор базы данных
+$dbconn = pg_connect("host=localhost dbname=hotelcrm user=hotel password=hotel")
+    or die('Не удалось соединиться: ' . pg_last_error());
+// Выполнение SQL-запроса
+$query = 'SELECT hotelcrm.add_hotel(' + $_POST["hName"] + ',' + $_POST["hAddress"]+ ')';
+$result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
+
+?>
