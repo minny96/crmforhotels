89e9fb430cddb66a4405cd03c42e4901e7613a39
minny96 <79872340869@ya.ru>
2019-04-25 21:52:18 +0300

    Рефакторинг, верстка страниц, блочность

---
+++ bs-binary-admin/menu.php
@@ -0,0 +1,28 @@
+<!-- /. NAV TOP  -->
+<nav class="navbar-default navbar-side" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav" id="main-menu">
+			<li class="text-center">
+                <img src="assets/img/find_user.png" class="user-image img-responsive"/>
+			</li>
+			<li>
+                <a  href="index.html"><i class="fa fa-dashboard fa-3x"></i>Главная</a>
+            </li>
+            <li>
+                <a  href="add_hostel.html"><i class="fa fa-desktop fa-3x"></i>Добавить хостел</a>
+            </li>
+            <li>
+                <a  href="add_room.html"><i class="fa fa-qrcode fa-3x"></i>Добавить комнату</a>
+            </li>
+			<li  >
+                <a  href="add_guest.html"><i class="fa fa-bar-chart-o fa-3x"></i>Добавить гостя</a>
+            </li>	
+            <li  >
+                <a  href="table.html"><i class="fa fa-table fa-3x"></i>хз</a>
+            </li>
+            <li>
+                <a   href="blank.html"><i class="fa fa-square-o fa-3x"></i>хз</a>
+            </li>	
+        </ul>
+    </div>
+</nav>
